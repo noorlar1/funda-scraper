3a6dacd1bb164521d8cf4c6912b839fa16c166a0
Roy Zwambag <royzwambag@gmail.com>
2021-01-22 21:52:57 +0100

    Save funda id as well to filter on it down the road

--- funda/items.py
+++ funda/items.py
@@ -2,11 +2,12 @@ import scrapy
 
 
 class BasicHouseItem(scrapy.Item):
-    url = scrapy.Field()
     address = scrapy.Field()
     zipcode = scrapy.Field()
     price = scrapy.Field()
     house_size = scrapy.Field()
     plot_size = scrapy.Field()
     rooms = scrapy.Field()
+    url = scrapy.Field()
     image = scrapy.Field()
+    funda_id = scrapy.Field()
